4f5153c1248195c4340e5d590d9930e9a796e0ce
Mauricio Bernardi <sisomau3@gmail.com>
2025-08-27 21:15:07 -0300

    Gerar Alertas by Fernando Vassoler

---
+++ app/_layout.tsx
@@ -0,0 +1,59 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { TaskProvider } from '@/src/hooks';
+
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
+
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  // Configura como as notificações se comportam quando o app está em foreground
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+useEffect(() => {
+  // Solicita permissão ao iniciar o app
+  async function requestNotificationPermission() {
+    const { status } = await Notifications.requestPermissionsAsync();
+    if (status !== 'granted') {
+      alert('Permissão para notificações foi negada.');
+    }
+  }
+
+  requestNotificationPermission();
+}, []);
+
+
+  return (
+    <TaskProvider>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="task-details" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </TaskProvider>
+  );
+}
